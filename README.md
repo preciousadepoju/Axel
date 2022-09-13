488033c40f71bd2a1e4bfeee5f43750beda7d3a7
Precious <addictcode6@gmail.com>
2022-09-14 00:00:49 +0100

    created two HTML files and also CSS files

---
+++ README.md
@@ -0,0 +1,1 @@
+###### Aiming for a better Advancement
---
+++ javascript/makeup.js
@@ -0,0 +1,52 @@
+// var gender ="bobrisky"
+// if (gender=="male") {
+//     alert("na man i be")
+// } if (gender=="female") {
+//     alert("gender that give apple to adam")
+// }
+// else{
+//     alert("the gender is female")
+// }
+
+// var myNumber = 10
+// else if (myNumber >2) {
+//     alert("greatr than 2")
+// }
+// if (myNumber >4) {
+//     alert("greatr than 2")
+// }
+// alert(6!=6)
+// var age = 40
+// if (age>10 && age>30) {
+//     alert("yes")
+// }
+// var age = 40
+// if (age>10 age>30) {
+//     alert("yes")
+// }
+// function myUser(){
+//     document.getElementById(userAge)
+//     var ageofusers=parseInt(userAge.value)
+// }
+
+// function div2(){
+//     console.log(
+//         div2.innerText ="JOY"
+//         div2.style.color="red"
+x
+//     )
+// }
+var randomNumber = Math.round(Math.random()*100)
+alert(randomNumber)
+
+var allStudents=[]
+function signup(){
+    var newStudent={
+        firstname:firstname.value,
+        lastname:lastname.value,
+        email:email.value,
+        password:password.value,
+    }
+    allStudents.push(newStudent)
+    console.log(allStudent[0].firstname)
+}
